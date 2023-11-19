832fe2a9e57076e05f667b3456e79fc24474900a
BelmontVsk <104748245+BelmontVsk@users.noreply.github.com>
2023-11-19 16:19:33 -0300

    uPDATE

---
+++ Exercicios js/Aula05/Ex008.js
@@ -0,0 +1,7 @@
+var vel = 5
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+if (vel > 60) {
+    console.log('Vocé ultrapassou limite màximo de velocidade , Vocé foi MULTADO!!')
+}
+
+console.log('Dirija sempre com cinto de segurança!')
---
+++ Exercicios js/Aula05/Ex009.js
@@ -0,0 +1,7 @@
+var pais = 'Russia'
+console.log(`Vivendo na ${pais}`)
+if (pais == 'Brasil') {
+    console.log('Vocé é Brasileiro')
+}else{
+    console.log('Vocé é Estrangeiro')
+}
